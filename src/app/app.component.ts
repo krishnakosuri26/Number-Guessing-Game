90a5ade4d0d8b53a39e65b6fd5d79b24b09e3893
krishnakosuri26 <157450361+krishnakosuri26@users.noreply.github.com>
2024-09-08 11:44:24 +0530

    number game 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'numbergame1';
+  title = 'Number Game';
+
+  myNumber: number = 0;
+  myNumberArray: number[] = [];
+  inputNumber: number = 0;
+  myMessage: string = "";
+  guessCount: number = 0;
+  maxGuesses: number = 10;
+
+  ngOnInit(){
+    this.startNewGame();
+  }
+
+  onClick(){
+    if (this.guessCount >= this.maxGuesses) {
+      this.myMessage = "Game over! You've used all your guesses.";
+      return;
+    }
+    this.guessCount++;
+    this.myNumberArray.push(this.inputNumber);
+
+    if(this.inputNumber == this.myNumber){
+      this.myMessage = "Your Guess is correct"
+      
+    }if(this.inputNumber > this.myNumber){
+      this.myMessage = "Your guess is high"
+      
+    }if(this.inputNumber < this.myNumber){
+      this.myMessage = "Your guess is low"
+    }
+
+    if (this.guessCount === this.maxGuesses) {
+      this.myMessage += " You've used all your guesses. The number was " + this.myNumber;
+    }
+
+  }
+
+  startNewGame() {
+    this.myNumber = Math.floor(Math.random() * 100) + 1;
+    this.myNumberArray = [];
+    this.inputNumber = 0;
+    this.myMessage = "";
+    this.guessCount = 0;
+  }
 }
